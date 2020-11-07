12191a0773ac24056d7cdbf0065a728604c2c4d8
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-07 18:34:00 +0100

    Update scripts.js
    took away incorrect <script> tag and added missing comma.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-<script>
 let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass' 'poison']},
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
   {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
   {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
 ];
-</script>
