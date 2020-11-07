14297d30662a6768bf0fa04734949a4cfd4c94d2
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-07 10:06:11 +0100

    Update scripts.js
    Three pokemons added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-
+<script>
+let pokemonList = [
+  {name: 'Bulbasaur', height: 0.7, types: ['grass' 'poison']},
+  {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+  {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
+];
+</script>
