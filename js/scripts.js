d1256961d41086a57459531a22b06a5709fb4e36
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-09 19:41:14 +0100

    Update scripts.js
    pokemonHeight variable added

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying']
-  },
-  {
-    name: 'Poliwrath',
-    height: 1.3,
-    types: ['water', 'fighting']
-  }
-];
+const pokemonList= [];
 
-//all pokemon details are written and if larger get a 'wow'
+pokemonList[0]={
+  name: 'Bulbasaur',
+  height: 0.7,
+  types: ['grass', 'poison']
+};
+
+pokemonList[1]={
+  name: 'Charizard',
+  height: 1.7,
+  types: ['fire', 'flying']
+};
+
+pokemonList[2]={
+  name: 'Poliwrath',
+  height: 1.3,
+  types: ['water', 'fighting']
+};
+
+let pokemonHeight = pokemonList[i].name + " (height: " + pokemonList[i].height + ") ";
+
 for (let i=0; i<pokemonList.length; i++){
-  if (pokemonList[i].height > 1.5) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "Wow, that's big! ");
+  if (pokemonList[i].height > 1.5)
+    document.write (pokemonHeight + "Wow, that's big! ");
   } else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ");
+    document.write(pokemonHeight);
   }
-}
+
+
+// for (let i=0; i<pokemonList.length; i++)
+//   if (pokemonList[i].height > 1.5){
+//     document.write (pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "--Wow, that's big! <br>");
+//   } else {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br>");
+//   }
