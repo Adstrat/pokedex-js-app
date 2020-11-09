8e8c1b66bc062e635c7d6e1906bd214fe006cb8c
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-09 16:38:44 +0100

    Update scripts.js
    conditional added

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
-pokemonList[0]={
-  name: 'Bulbasaur',
-  height: 0.7,
-  types: ['grass', 'poison']
-};
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying']
+  },
+  {
+    name: 'Poliwrath',
+    height: 1.3,
+    types: ['water', 'fighting']
+  }
+];
 
-pokemonList[1]={
-  name: 'Charizard',
-  height: 1.7,
-  types: ['fire', 'flying']
-};
-
-pokemonList[2]={
-  name: 'Poliwrath',
-  height: 1.3,
-  types: ['water', 'fighting']
-};
+//all pokemon details are written and if larger get a 'wow'
+for (let i=0; i<pokemonList.length; i++){
+  if (pokemonList[i].height > 1.5) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "Wow, that's big! ");
+  } else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ");
+  }
+}
