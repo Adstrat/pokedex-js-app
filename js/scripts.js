2d40a69bc7667b6be79a32a8fa21088981370d6c
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-09 09:29:44 +0100

    Update scripts.js
    changed pokemon format

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-  {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
-];
+pokemonList[0]={
+  name: 'Bulbasaur',
+  height: 0.7,
+  types: ['grass', 'poison']
+};
+
+pokemonList[1]={
+  name: 'Charizard',
+  height: 1.7,
+  types: ['fire', 'flying']
+};
+
+pokemonList[2]={
+  name: 'Poliwrath',
+  height: 1.3,
+  types: ['water', 'fighting']
+};
