9b0366602a55efdcbb58c2be2b0000a884b1d24e
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-14 20:13:40 +0100

    Update scripts.js
    IIFE created

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
-const pokemonList= [];
+let pokemonRepository = ( () => {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying']
+    },
+    {
+      name: 'Poliwrath',
+      height: 1.3,
+      types: ['water', 'fighting']
+    }
+  ];
 
-pokemonList[0]={
-  name: 'Bulbasaur',
-  height: 0.7,
-  types: ['grass', 'poison']
-};
+  function getAll(){
+    return pokemonList;
+  }
 
-pokemonList[1]={
-  name: 'Charizard',
-  height: 1.7,
-  types: ['fire', 'flying']
-};
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
 
-pokemonList[2]={
-  name: 'Poliwrath',
-  height: 1.3,
-  types: ['water', 'fighting']
-};
+  return {
+    getAll: getAll,
+    add: add
+  };
 
-pokemonList.forEach(function(pokemon) {
+})();
+
+
+pokemonRepository.getAll.forEach (pokemon => {
   let pokemonHeight = pokemon.name + " (height: " + pokemon.height + ") ";
   if (pokemon.height > 1.5){
     document.write (pokemonHeight + "Wow, that's big! <br>");
