ce5c2e83de2e16fa292a2da83d44cbb80c7e9380
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-11-21 22:02:37 +0100

    Update scripts.js
    changed to use promises to fetch data from API

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,25 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'Bulbasaur',
-      height: 0.7,
-      types: ['grass', 'poison']
-    },
-    {
-      name: 'Charizard',
-      height: 1.7,
-      types: ['fire', 'flying']
-    },
-    {
-      name: 'Poliwrath',
-      height: 1.3,
-      types: ['water', 'fighting']
-    }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  function getAll(){
-    return pokemonList;
-  }
-
+  //validates pokemon, then pushes to pokemonList
   function add(pokemon){
-    if (typeof pokemon === 'object' && Object.keys === ['name', 'height', 'types']) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'detailsUrl' in pokemon
+    ){
       pokemonList.push(pokemon);
     } else {
       console.log("Pokemon is not correct");
     }
   }
 
+  function getAll(){
+    return pokemonList;
+  }
+
+  //creates button with event listener (DOM manipulation) for "showDetails" function
   function addListItem(pokemon){
     let pokemonList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
@@ -37,25 +28,70 @@ let pokemonRepository = (function () {
     button.classList.add('pokemon-button');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    //button triggers pokemon details when clicked
     button.addEventListener('click', function(event){
-    showDetails(pokemon);
+      showDetails(pokemon);
     })
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon);
+  //fetches the promise from API, validates in "add" function, then pushes to pokemonList
+  function loadList() {
+    return fetch(apiUrl).then(function (response){
+      //returns parsed data
+      return response.json();
+      //callback for second promise, which gets the desired data
+    }).then(function (json) {
+      json.results.forEach(function (item){
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        //loads all pokemon details in console
+        console.log(pokemon);
+      });
+    }).catch(function (e){
+      console.error(e);
+    })
   }
 
+  //fetches specific details of pokemon. When addListItem button is clicked
+  // it logs that pokemon in the console (via showDetails function)
+  function loadDetails(item){
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response){
+      return response.json();
+    }).then(function (details){
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e){
+      console.error(e);
+    });
+  }
+
+  //logs pokemon in console when button clicked (addListItem function)
+  function showDetails(pokemon){
+    loadDetails(pokemon).then(function(){
+      console.log(pokemon);
+    });
+  }
+
+  //external access to functions
   return {
-    getAll: getAll,
     add: add,
-    addListItem: addListItem
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 
 })();
 
-
-pokemonRepository.getAll().forEach (function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+//calls pokemonList which is updated from loadList, in order to..
+pokemonRepository.loadList().then(function() {
+  //..request each pokemon to create button in addListItem
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
