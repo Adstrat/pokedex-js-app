db26f5e30aabde03ada06f45546c72d9e4f4ca18
Adstrat <73385073+Adstrat@users.noreply.github.com>
2020-12-07 19:33:13 +0100

    Revert "Update scripts.js"
    This reverts commit ac40befc5abfc8f061956f6eb2991e0a083b36af.

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,18 +1,18 @@
 let pokemonRepository = (function() {
   let t = [],
-    e = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    e = "https://pokeapi.co/api/v2/pokemon/?limit=150";
   function n(t) {
     o(t).then(function() {
       !(function(t) {
-        let e = $('.modal-title'),
-          n = $('.modal-body');
+        let e = $(".modal-title"),
+          n = $(".modal-body");
         e.empty(), n.empty();
-        let o = $('<h1>' + t.name + '</h1>'),
+        let o = $("<h1>" + t.name + "</h1>"),
           i = $('<img class="modal-image" style="width:50%">');
-        i.attr('src', t.imageUrl);
-        let a = $('<p>Height: ' + t.height + '</p>'),
-          l = $('<p>Weight: ' + t.weight + '</p>'),
-          p = $('<p>Types: ' + t.types.toString().replace(',', ', ') + '</p>');
+        i.attr("src", t.imageUrl);
+        let a = $("<p>Height: " + t.height + "</p>"),
+          l = $("<p>Weight: " + t.weight + "</p>"),
+          p = $("<p>Types: " + t.types.toString().replace(",", ", ") + "</p>");
         e.append(o), n.append(i), n.append(a), n.append(l), n.append(p);
       })(t);
     });
@@ -38,15 +38,15 @@ let pokemonRepository = (function() {
       return t;
     },
     addListItem: function(t) {
-      let e = $('.pokemon-list'),
-        o = $('<li></li>'),
-        i = $('<button>' + t.name + '</button>');
-      i.addClass('btn', 'btn-primary'),
-        i.attr('data-toggle', 'modal'),
-        i.attr('data-target', '#pokemon-modal'),
+      let e = $(".pokemon-list"),
+        o = $("<li></li>"),
+        i = $("<button>" + t.name + "</button>");
+      i.addClass("btn", "btn-primary"),
+        i.attr("data-toggle", "modal"),
+        i.attr("data-target", "#pokemon-modal"),
         o.append(i),
         e.append(o),
-        i.on('click', function() {
+        i.on("click", function() {
           n(t);
         });
     },
@@ -58,9 +58,9 @@ let pokemonRepository = (function() {
         .then(function(e) {
           e.results.forEach(function(e) {
             !(function(e) {
-              'object' == typeof e && 'name' in e && 'detailsUrl' in e
+              "object" == typeof e && "name" in e && "detailsUrl" in e
                 ? t.push(e)
-                : console.log('Pokemon is not correct');
+                : console.log("Pokemon is not correct");
             })({ name: e.name, detailsUrl: e.url });
           });
         })
