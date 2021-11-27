8460120e4d0f1f419438edb145558f501a505bdf
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-11-27 20:25:49 +0100

    Update game-scripts.js
    code refactored to eliminate duplication of large event listener.

--- src/js/game-scripts.js
+++ src/js/game-scripts.js
@@ -34,7 +34,7 @@ const getHeightDesktop = () => {
   return height;
 }
 
-
+let mobileView = window.matchMedia( "(max-width: 480px)" );
 
 // puts the pokemon image in a ramdon place in the landscape
 const showPokemon = ( image ) => {
@@ -45,7 +45,7 @@ const showPokemon = ( image ) => {
   img.classList = "catch-pokemon";
 
   // media query to set random area for pokemon to appear
-  let mobileView = window.matchMedia( "(max-width: 480px)" );
+
   if ( mobileView.matches ) {
     img.style.left = getRandomNumber( getWidthMobile() ) + "px";
     img.style.top = getRandomNumber( getHeightMobile() ) + "px";
@@ -58,50 +58,26 @@ const showPokemon = ( image ) => {
   gameContainer.append( img );
 }
 
-// MobileView - makes one pokemon appear on first click in landscape, then function disabled
-landscapeMobile.addEventListener( "click", () => {
-  showPokemon( "img/pokemon.svg" );
-  // starts 10 second timer - removes last pokemon and reveals score
-  setTimeout( () => {
-    document.querySelector( ".catch-pokemon" ).remove();
-    result.classList.remove( "d-none" );
-    playAgain.classList.remove( "d-none" );
-    landscapeMobile.style.filter = "grayscale(60%)";
-    landscapeMobile.style.opacity = 0.9;
-    // scrolls through score 
-    let output = 0;
-    const timer = setInterval( () => {
-      result.querySelector( "span" ).textContent = `${ output }`;
-      if ( output === pokes ) {
-        clearInterval( timer );
-      } else {
-        output++;
-      }
-    }, 80 );
-  }, 10000 );
-  // using event delegation to remove pokemon when clicked and reveal a new one, increases pokes
-  gameContainer.addEventListener( "click", e => {
-    if ( e.target.className === "catch-pokemon" ) {
-      pokes++
-      e.target.remove();
-      showPokemon( "img/pokemon.svg" );
-    }
-  } )
 
-}, {
-  once: true
-} );
+
+let landscape;
+if ( mobileView.matches ) {
+  landscape = landscapeMobile;
+} else {
+  landscape = landscapeDesktop;
+}
+
 
 // DesktopView - makes one pokemon appear on first click in landscape, then function disabled
-landscapeDesktop.addEventListener( "click", () => {
+landscape.addEventListener( "click", () => {
   showPokemon( "img/pokemon.svg" );
   // starts 10 second timer - removes last pokemon and reveals score
   setTimeout( () => {
     document.querySelector( ".catch-pokemon" ).remove();
     result.classList.remove( "d-none" );
     playAgain.classList.remove( "d-none" );
-    landscapeDesktop.style.filter = "grayscale(60%)";
-    landscapeDesktop.style.opacity = 0.9;
+    landscape.style.filter = "grayscale(60%)";
+    landscape.style.opacity = 0.9;
     // scrolls through score 
     let output = 0;
     const timer = setInterval( () => {
